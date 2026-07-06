7fa1806b6de9d5ea5618a131e749c9775ef043ba
Jonathan <jonathan.academic1@gmail.com>
2026-07-06 12:03:07 +0300

    Improved Settings Page
    Improved settings page ui, added setting to change accent color

--- settings.py
+++ settings.py
@@ -1,26 +1,37 @@
-from PySide6.QtWidgets import QFrame, QVBoxLayout, QHBoxLayout
-from qfluentwidgets import TitleLabel, SwitchButton, setTheme, Theme, BodyLabel
-
+from PySide6.QtWidgets import QFrame, QVBoxLayout
+from PySide6.QtGui import QColor
+from qfluentwidgets import TitleLabel, setTheme, Theme, SwitchSettingCard, SettingCardGroup, FluentIcon as FIF, PushSettingCard, setThemeColor, ColorDialog
 
 class settings(QFrame):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.setObjectName("Settings")
+        self.setObjectName("settings")
         self.title = TitleLabel("Settings")
 
-        button = SwitchButton()
-        button.checkedChanged.connect(lambda checked: setTheme(Theme.DARK if checked else Theme.LIGHT))
-        button.setChecked(True)
-        label = BodyLabel("Dark Theme")
+        self.themecard = SwitchSettingCard(icon=FIF.BRUSH, title="Dark Mode", content="Change the theme of the app", parent=self)
+        self.themecard.switchButton.setChecked(False)
+        self.themecard.checkedChanged.connect(lambda checked: setTheme(Theme.DARK if checked else Theme.LIGHT))
 
-        toggle_layout = QHBoxLayout()
-        toggle_layout.addWidget(label)
-        toggle_layout.addWidget(button)
-        toggle_layout.addStretch()
+        self.accentcard = PushSettingCard(text = "Choose Color", icon=FIF.PALETTE, title="Accent Color", content="Change the primary accent colour", parent=self)
+        self.accentcard.clicked.connect(self.opencolorpicker)
+
+        self.personalization = SettingCardGroup("Personalization", self)
+        self.personalization.addSettingCard(self.themecard)
+        self.personalization.addSettingCard(self.accentcard)
+
 
         layout = QVBoxLayout(self)
+        layout.setContentsMargins(36, 22, 36, 22)
+        layout.setSpacing(10)
+
         layout.addWidget(self.title)
-        layout.addSpacing(20)
-        layout.addLayout(toggle_layout)
+        layout.addSpacing(15)
+        layout.addWidget(self.personalization)
         layout.addStretch()
+
         self.setLayout(layout)
+
+    def opencolorpicker(self):
+        w = ColorDialog(QColor("#0078D4"), "Choose Accent Color", self.window(), enableAlpha=False)
+        w.colorChanged.connect(lambda color: setThemeColor(color))
+        w.exec()
